67be1de3b46eb3454a143f80fde5fae2d4c9816e
maris <marisprimoon@gmail.com>
2024-03-18 20:03:33 -0300

    atualizações

--- src/MainScreens/SavedPasswordsScreen.jsx
+++ src/MainScreens/SavedPasswordsScreen.jsx
@@ -1,3 +1,5 @@
+//ARQUIVO PARA EXIBIR AS SENHAS APÓS SALVAR 
+
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
@@ -10,9 +12,11 @@ const SavedPasswordsScreen = () => {
   // Função para buscar as senhas do banco de dados SQLite
   const buscarSenhasSalvas = async () => {
     try {
-      const connection = await getConnection();
-      const senhaRepository = connection.getRepository(GeraSenha);
-      const senhasSalvas = await senhaRepository.find();
+      const connection = await getConnection();//obtem conexão com o bd
+      const senhaRepository = connection.getRepository(GeraSenha); //obtém o repositório das senhas usando getRepositoty(GeraSenha)
+      const senhasSalvas = await senhaRepository.find(); //busca as senhas usando senhaRepository.find()
+      //e adiciona a senha ao estado, enquanto adiciona uma propriedade isVisible a cada senha para controlar a visibilidade.
+      // Adiciona uma propriedade 'isVisible' a cada senha
       setSenhas(senhasSalvas.map(senha => ({ ...senha, isVisible: false })));
     } catch (error) {
       console.error('Erro ao buscar senhas do banco de dados:', error);
@@ -24,17 +28,19 @@ const SavedPasswordsScreen = () => {
     buscarSenhasSalvas();
   }, []);
 
-  // Função para alternar a visibilidade da senha
-  const togglePasswordVisibility = index => {
-    setSenhas(prevSenhas => {
-      return prevSenhas.map((senha, i) => {
-        if (i === index) {
-          return { ...senha, isVisible: !senha.isVisible };
-        }
-        return senha;
-      });
+// Função para alternar a visibilidade da senha, chamada quando o usuário pressiona o botão de olho 
+const togglePasswordVisibility = index => {
+  setSenhas(prevSenhas => {
+    // Cria uma nova lista de senhas com a visibilidade alternada para a senha específica
+    //mapeia sobre o estado anterior das senhas e alterna a visibilidade para a senha específica
+    return prevSenhas.map((senha, i) => {
+      if (i === index) {
+        return { ...senha, isVisible: !senha.isVisible };
+      }
+      return senha;
     });
-  };
+  });
+};
 
   // Função para excluir uma senha do banco de dados
   const excluirSenha = async index => {
@@ -54,16 +60,20 @@ const SavedPasswordsScreen = () => {
   return (  
     <View style={styles.container}>
       <ScrollView contentContainerStyle={styles.scrollContent}>
+        {/* Renderizar cada senha como um quadrado, para isso mapeia o estado da senha*/}
         {senhas.map((senha, index) => (
+          //rendereiza cada senha como um quadrado
           <View key={index} style={[styles.passwordContainer, senha.isVisible && styles.visibleContainer]}>
             <Text style={[styles.text, senha.isVisible && styles.whiteText]}>
               {senha.isVisible ? senha.password : '•'.repeat(senha.password.length)}
             </Text>
+            {/* Botão para alternar a visibilidade da senha */}
             <TouchableOpacity onPress={() => togglePasswordVisibility(index)} style={styles.eyeButton}>
               <Ionicons name={senha.isVisible ? 'eye-off-outline' : 'eye-outline'} size={24} color="#FFFFFF" />
             </TouchableOpacity>
             {/* Botão para excluir a senha */}
             <TouchableOpacity onPress={() => excluirSenha(index)} style={styles.deleteButton}>
+              {/* Ícone de olho para mostrar ou ocultar a senha */}
               <Ionicons name="trash-outline" size={24} color="#FFFFFF" />
             </TouchableOpacity>
           </View>

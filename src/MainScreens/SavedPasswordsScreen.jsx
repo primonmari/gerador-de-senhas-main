d8cf2a6d7def1229d29d6dca4776ec51b02de4c7
maris <marisprimoon@gmail.com>
2024-03-18 23:29:06 -0300

    atual senhas bd

--- src/MainScreens/SavedPasswordsScreen.jsx
+++ src/MainScreens/SavedPasswordsScreen.jsx
@@ -1,3 +1,5 @@
+//ARQUIVO PARA EXIBIR AS SENHAS APÓS SALVAR 
+
 import React, { useState, useEffect } from 'react';
 import { View, Text, StyleSheet, TouchableOpacity, ScrollView } from 'react-native';
 import { Ionicons } from '@expo/vector-icons';
@@ -26,23 +28,40 @@ const SavedPasswordsScreen = () => {
     buscarSenhasSalvas();
   }, []);
 
-  // Função para alternar a visibilidade da senha, chamada quando o usuário pressiona o botão de olho 
-  const togglePasswordVisibility = index => {
-    setSenhas(prevSenhas => {
-      // Cria uma nova lista de senhas com a visibilidade alternada para a senha específica
-      //mapeia sobre o estado anterior das senhas e alterna a visibilidade para a senha específica
-      return prevSenhas.map((senha, i) => {
-        if (i === index) {
-          return { ...senha, isVisible: !senha.isVisible };
-        }
-        return senha;
-      });
+// Função para alternar a visibilidade da senha, chamada quando o usuário pressiona o botão de olho 
+const togglePasswordVisibility = index => {
+  setSenhas(prevSenhas => {
+    // Cria uma nova lista de senhas com a visibilidade alternada para a senha específica
+    //mapeia sobre o estado anterior das senhas e alterna a visibilidade para a senha específica
+    return prevSenhas.map((senha, i) => {
+      if (i === index) {
+        return { ...senha, isVisible: !senha.isVisible };
+      }
+      return senha;
     });
+  });
+};
+
+  // Função para excluir uma senha do banco de dados
+  const excluirSenha = async index => {
+    try {
+      const connection = await getConnection();
+      const senhaRepository = connection.getRepository(GeraSenha);
+      const senhaExcluida = senhas[index];
+      await senhaRepository.delete(senhaExcluida.id); // Delete a senha com base no ID
+      // Atualize o estado das senhas para refletir a exclusão
+      setSenhas(prevSenhas => prevSenhas.filter((_, i) => i !== index));
+      console.log('Senha excluída com sucesso!');
+    } catch (error) {
+      console.error('Erro ao excluir a senha:', error);
+    }
   };
 
   return (  
     <View style={styles.container}>
-      <ScrollView contentContainerStyle={styles.scrollContent}>
+      <ScrollView contentContainerStyle={styles.scrollContent}
+        showsVerticalScrollIndicator={false} // Define esta propriedade como false
+      >
         {/* Renderizar cada senha como um quadrado, para isso mapeia o estado da senha*/}
         {senhas.map((senha, index) => (
           //rendereiza cada senha como um quadrado
@@ -52,8 +71,12 @@ const SavedPasswordsScreen = () => {
             </Text>
             {/* Botão para alternar a visibilidade da senha */}
             <TouchableOpacity onPress={() => togglePasswordVisibility(index)} style={styles.eyeButton}>
-              {/* Ícone de olho para mostrar ou ocultar a senha */}
               <Ionicons name={senha.isVisible ? 'eye-off-outline' : 'eye-outline'} size={24} color="#FFFFFF" />
+            </TouchableOpacity>
+            {/* Botão para excluir a senha */}
+            <TouchableOpacity onPress={() => excluirSenha(index)} style={styles.deleteButton}>
+              {/* Ícone de olho para mostrar ou ocultar a senha */}
+              <Ionicons name="trash-outline" size={24} color="#FFFFFF" />
             </TouchableOpacity>
           </View>
         ))}
@@ -65,17 +88,20 @@ const SavedPasswordsScreen = () => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#FFFFFF',
     marginLeft: 10,
     marginRight: 10,
+    
   },
   scrollContent: {
     justifyContent: 'center',
     alignItems: 'center',
-    
   },
   passwordContainer: {
     flexDirection: 'row',
     backgroundColor: '#274135',
+    borderBottomWidth: 1,
+    borderBottomColor: 'transparent',
     height: 50,
     width: '100%',
     marginVertical: 8,
@@ -93,10 +119,14 @@ const styles = StyleSheet.create({
   },
   eyeButton: {
     marginLeft: 'auto',
-    marginRight: 10, // Ajuste conforme necessário para o espaçamento do botão à direita
+    marginRight: 10,
+  },
+  deleteButton: {
+    marginLeft: 10,
+    marginRight: 10,
   },
   visibleContainer: {
-    backgroundColor: '#A2D9A1', // Cor do contêiner quando a senha está visível
+    backgroundColor: '#A2D9A1',
   },
 });
 
